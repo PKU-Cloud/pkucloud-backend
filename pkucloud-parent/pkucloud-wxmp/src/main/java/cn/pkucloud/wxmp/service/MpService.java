fc84ff9f5e887cf19ff8a590821968bbf469205d
mryaopku <56622736+mryaopku@users.noreply.github.com>
2020-12-31 01:25:03 +0800

    implements WeChat Push

--- pkucloud-parent/pkucloud-wxmp/src/main/java/cn/pkucloud/wxmp/service/MpService.java
+++ pkucloud-parent/pkucloud-wxmp/src/main/java/cn/pkucloud/wxmp/service/MpService.java
@@ -1,5 +1,6 @@
 package cn.pkucloud.wxmp.service;
 
+import cn.pkucloud.common.Result;
 import cn.pkucloud.wxmp.crypto.CryptoException;
 import cn.pkucloud.wxmp.dto.wx.xml.XmlRequest;
 import cn.pkucloud.wxmp.dto.wx.xml.XmlResponse;
@@ -9,4 +10,8 @@ public interface MpService {
     String echo(String signature, String echostr, int timestamp, String nonce);
 
     XmlResponse msgHandler(String signature, int timestamp, String nonce, String openid, String encrypt_type, String msg_signature, XmlRequest request) throws JsonProcessingException, CryptoException;
+
+    Result<?> getSubscribe(Long id);
+
+    Result<?> sendAnswerMsg(String[] ids, String first, String title, String userName, String time, String remark);
 }
